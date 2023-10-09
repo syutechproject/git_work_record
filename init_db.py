fcc03a22a6a8fbe674aca07cfef5a77a592bb22f
syutechproject <bluetreeama@gmail.com>
2023-10-10 08:56:38 +0900

    make it better

--- init_db.py
+++ init_db.py
@@ -19,18 +19,18 @@ rev_hashed_password = hashed_password.decode()
 #     db.close()
 
 
-# with app.app_context():
-#     db = get_db()
-#     cur = db.cursor()
-#     cur.execute(
-#         "insert into USERS (user_id, password, name)\
-#             values('{user_id}', '{password}', '{name}')".format(
-#             user_id=userid,
-#             password=rev_hashed_password,
-#             name=name)
-#         )
-#     db.commit()
-#     db.close()
+with app.app_context():
+    db = get_db()
+    cur = db.cursor()
+    cur.execute(
+        "insert into USERS (user_id, password, name)\
+            values('{user_id}', '{password}', '{name}')".format(
+            user_id=userid,
+            password=rev_hashed_password,
+            name=name)
+        )
+    db.commit()
+    db.close()
 
 # with app.app_context():
 #     db = get_db()
@@ -39,25 +39,25 @@ rev_hashed_password = hashed_password.decode()
 #     db.close()
 
 
-with app.app_context():
-    db = get_db()
-    cur = db.cursor()
-    cur.execute("CREATE TABLE USERS\
-               ( user_id text primary key not null,\
-                 password text not null,\
-                 name text not null );")
-    db.commit()
-    db.close()
-
-
 # with app.app_context():
 #     db = get_db()
 #     cur = db.cursor()
-#     cur.execute("CREATE TABLE CLOCKIN_INFO \
-#                (user_id text not null, clockin_date text not null,\
-#                 clockin_div text not null, serno text not null, \
-#                clockin_time text not null, \
-#                primary key (user_id,clockin_date,clockin_div,serno), \
-#                FOREIGN KEY (user_id) REFERENCES USERS (user_id) );")
+#     cur.execute("CREATE TABLE USERS\
+#                ( user_id text primary key not null,\
+#                  password text not null,\
+#                  name text not null );")
 #     db.commit()
 #     db.close()
+
+
+with app.app_context():
+    db = get_db()
+    cur = db.cursor()
+    cur.execute("CREATE TABLE CLOCKIN_INFO \
+               (user_id text not null, clockin_date text not null,\
+                clockin_div text not null, serno text not null, \
+               clockin_time text not null, \
+               primary key (user_id,clockin_date,clockin_div,serno), \
+               FOREIGN KEY (user_id) REFERENCES USERS (user_id) );")
+    db.commit()
+    db.close()
